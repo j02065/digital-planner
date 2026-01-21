9c278fab48a0d048347b52e112c98f80dee9c355
j02065 <161518225+j02065@users.noreply.github.com>
2026-01-21 12:59:03 -0600

    Update cloud-sync.js

--- cloud-sync.js
+++ cloud-sync.js
@@ -37,10 +37,10 @@ class CloudStorageSync {
     async authenticate() {
         const configs = {
             box: {
-                clientId: 'hg3q6etueooioxakdss6utwnj2qb9gty', // ← PUT YOUR CLIENT ID HERE
+                clientId: 'nya3rh3jh0mhtxp4cx45gaam7kmg1zrb', // ← PUT YOUR CLIENT ID HERE
                 authUrl: 'https://account.box.com/api/oauth2/authorize',
                 // ↓↓↓ CHANGE THIS TO YOUR EXACT GITHUB PAGES URL ↓↓↓
-                redirectUri: 'https://j02065.github.io/digital-planner'
+                redirectUri: 'https://j02065.github.io/digital-planner/'
                 // Example: 'https://john123.github.io/digital-planner'
             },
             onedrive: {
